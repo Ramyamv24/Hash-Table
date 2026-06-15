456cb12587149fe7010163525d255d714069da85
Ramyamv24 <ramyavani689@gmail.com>
2026-06-15 16:44:54 +0530

    15-06-2026

---
+++ Top K Frequent Elements.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        count = Counter(nums)
+
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in count.items():
+            bucket[freq].append(num)
+
+        res = []
+
+        for i in range(len(nums), 0, -1):
+            for num in bucket[i]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
+
+
+# Main Part
+nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
+k = int(input("Enter k: "))
+
+obj = Solution()
+result = obj.topKFrequent(nums, k)
+
+print("Top", k, "frequent elements:", result)
